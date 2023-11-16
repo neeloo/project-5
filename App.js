efc4fe727528fdb81e35a03b54ee4bbc504f00be
neeloo <123488468+neeloo@users.noreply.github.com>
2023-11-16 22:44:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import ImageGenerater from './Component/ImageGernerater/ImageGenerater';
+
+function App() {
+  return (
+
+    <div>
+      <ImageGenerater/>
+    </div>
+    
+  );
+}
+
+export default App;
